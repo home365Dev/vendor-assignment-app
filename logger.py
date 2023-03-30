efd81fec3de9004b454a00dca32b58c8da2e4db1
NoamHome365 <noam@home365.co>
2023-03-30 13:51:21 +0300

    fix

--- logger.py
+++ logger.py
@@ -4,21 +4,21 @@ from logging import *
 from cloud_watch_logs import CloudWatchLogger
 import configuration as config
 
-logger = CloudWatchLogger(log_group='Vendor_Assignment_app', log_stream='conversation')
+logger = CloudWatchLogger(log_group='Vendor_Assignment_App', log_stream='conversation')
 
-name_str = '[{}]'.format(str(config.ENV_PREFIX))
-basicConfig(
-    format='{} %(asctime)s %(levelname)s [%(module)s.%(funcName)s:%(lineno)d] %(message)s'.format(name_str),
-    datefmt='%H:%M:%S',
-    level=INFO)
-
-logger = getLogger(__name__)
-# logger.addHandler(StreamHandler(stream=sys.stdout))
-# if not config.IS_TEST:
-#     logger.addHandler(watchtower.CloudWatchLogHandler())
-
-if config.IS_TEST:
-    logger.setLevel(logging.DEBUG)
-else:
-    logger.setLevel(logging.INFO)
+# name_str = '[{}]'.format(str(config.ENV_PREFIX))
+# basicConfig(
+#     format='{} %(asctime)s %(levelname)s [%(module)s.%(funcName)s:%(lineno)d] %(message)s'.format(name_str),
+#     datefmt='%H:%M:%S',
+#     level=INFO)
+#
+# logger = getLogger(__name__)
+# # logger.addHandler(StreamHandler(stream=sys.stdout))
+# # if not config.IS_TEST:
+# #     logger.addHandler(watchtower.CloudWatchLogHandler())
+#
+# if config.IS_TEST:
+#     logger.setLevel(logging.DEBUG)
+# else:
+#     logger.setLevel(logging.INFO)
 logger.info("Log has started")
